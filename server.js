bc30656b0c42ca4d0afb0c0a34eb00ca5ac2a23e
millsjohnathan56-afk <millsjohnathan56@gmail.com>
2026-08-25 18:10:18 -0400

    Update server.js to conditionally start server
    Modify server.js to listen only when running locally and export the app.

--- server.js
+++ server.js
@@ -4,12 +4,19 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve everything inside the public folder
 app.use(express.static(path.join(__dirname, "public")));
 
+// Send index.html for other routes
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`MyGame Universe running on port ${PORT}`);
-});
+// Only listen when running locally
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`MyGame Universe running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
