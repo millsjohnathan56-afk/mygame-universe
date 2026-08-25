7b837533482a24c7539b76685e0b353c207c027a
millsjohnathan56-afk <millsjohnathan56@gmail.com>
2026-08-25 18:28:51 -0400

    Add JSON parsing middleware to server

--- server.js
+++ server.js
@@ -4,15 +4,13 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve everything inside the public folder
+app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-// Send index.html for other routes
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// Only listen when running locally
 if (require.main === module) {
   app.listen(PORT, () => {
     console.log(`MyGame Universe running on port ${PORT}`);
